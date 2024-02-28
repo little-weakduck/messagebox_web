be122aa7317fcc8e8de12494ea06c9a416aedf88
Little Weak_Duck <yinjiab@gmail.com>
2024-02-28 21:51:29 +0800

    feat: app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="留言板！" />
+      <MyButton class="button" type="success" @click="showInput = true"> 绑定管理员身份</MyButton>
     </div>
   </header>
-
   <main>
-    <TheWelcome />
-    <CommentComponent  :comment="comment"/>
+    <!-- <TheWelcome /> -->
+    <Divider class="divider-width" />
+    <div class="comment-list">
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+      <CommentComponent :comment="comment" />
+    </div>
   </main>
+  <Dialog v-model="showInput" @close="showInput = false" @confirm="bindAdmin">
+    <template v-slot:default>
+      <h2>请输入管理员密码</h2>
+    </template>
+  </Dialog>
 </template>
 
 <script setup lang="ts">
@@ -18,13 +39,31 @@ import HelloWorld from './components/HelloWorld.vue'
 import CommentComponent from './components/CommentComponent.vue';
 import TheWelcome from './components/TheWelcome.vue'
 import type { Comment } from './types/comment';
+import Divider from './components/Divider.vue';
+import MyButton from './components/MyButton.vue';
+import { ref } from 'vue';
+import Dialog from './components/Dialog.vue';
+
+const adminPassword = ref("")
+const showInput = ref(true)
+
+const bindAdmin = () => {
+
+}
 
 const comment: Comment = {
   id: "1",
   title: 'This is a comment',
   content: 'This is the content of the comment',
-  updateAt: new Date()
-
+  author: 'Little Weak_Duck',
+  updateAt: new Date(),
+  parent: {
+    id: "2",
+    title: 'This is a comment',
+    content: 'This is the content of the comment',
+    author: 'Little Weak_Duck',
+    updateAt: new Date()
+  }
 }
 </script>
 
@@ -33,9 +72,27 @@ header {
   line-height: 1.5;
 }
 
+header .wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.button {
+  align-self: flex-end;
+}
+
+.comment-list {
+  flex-grow: 1;
+  overflow: scroll;
+  height: 100%;
 }
 
 @media (min-width: 1024px) {
@@ -51,8 +108,18 @@ header {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
+    align-items: center;
+    justify-content: center;
     flex-wrap: wrap;
   }
+
+  .divider-width {
+    display: none;
+  }
+
+  main {
+    height: 100vh;
+  }
+
 }
 </style>
