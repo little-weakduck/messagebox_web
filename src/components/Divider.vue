9a2341cdbd7f49e7c0cca9ff96dd3fef9de9d415
Little Weak_Duck <yinjiab@gmail.com>
2024-02-28 21:51:01 +0800

    feat: divider 组件

---
+++ src/components/Divider.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Author: Little Weak_Duck
+ * @Date: 2024-02-25 12:25:45
+ * @LastEditors: Little Weak_Duck
+ * @LastEditTime: 2024-02-25 12:27:09
+ * @FilePath: /src/components/Divider.vue
+ * @Description:  divider
+-->
+<template>
+  <div :style="{ width: width, height: height }" class="divider"></div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+    width: {
+        type: String,
+        default: "100%",
+    },
+    height: {
+        type: String,
+        default: "1px",
+    },
+});
+</script>
+
+<style scoped>
+.divider {
+  background-color: #ccc;
+}
+</style>
